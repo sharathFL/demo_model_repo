172b0aa3de407d8884f3621d554ca62146459c77
sharathFL <sharath@finiteloop.io>
2024-10-21 01:19:06 +0530

    improved model C

--- src/models.py
+++ src/models.py
@@ -4,10 +4,11 @@ class DummyModelV3(torch.nn.Module):
     def __init__(self):
         super(DummyModelV3, self).__init__()
         self.layer = torch.nn.Linear(10, 1)
-        self.version = "Model Version 3.0"  # New version string for V3
+        self.version = "Model Version 3.2"  # New version string for V3
 
     def forward(self, x):
-        return self.layer(x)
+        # x=self.layer(x)
+        return self.layer(x*x)
     
     def get_version(self):
         return self.version
